321b59c2b2221faa4a2a961a2461663be883ba03
kohei <kouhei.ikeda@veriserve.co.jp>
2023-05-10 17:55:09 +0900

    タスク管理機能を追加

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -5,12 +5,15 @@ import { LogoutIcon } from '@heroicons/react/solid'
 import { Layout } from '@/components/Layout'
 import { UserInfo } from '@/components/UserInfo'
 import { useQueryClient } from '@tanstack/react-query'
+import { TaskForm } from '@/components/TaskForm'
+import { TaskList } from '@/components/TaskList'
 
 const Dashboard: NextPage = () => {
   const router = useRouter()
   const queryClient = useQueryClient()
   const logout = async () => {
     queryClient.removeQueries(['user'])
+    queryClient.removeQueries(['tasks'])
     await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`)
     router.push('/')
   }
@@ -22,6 +25,8 @@ const Dashboard: NextPage = () => {
         onClick={logout}
       />
       <UserInfo />
+      <TaskForm />
+      <TaskList />
     </Layout>
   )
 }
---
+++ store/index.ts
@@ -0,0 +1,24 @@
+import craete from 'zustand'
+import { EditedTask } from '@/types'
+
+type State = {
+  editedTask: EditedTask
+  updatedEditedTask: (payload: EditedTask) => void
+  resetEditedTask: () => void
+}
+
+const useStore = craete<State>((set) => ({
+  editedTask: { id: 0, title: '', description: '' },
+  updatedEditedTask: (payload) =>
+    set({
+      editedTask: {
+        id: payload.id,
+        title: payload.title,
+        description: payload.description,
+      },
+    }),
+  resetEditedTask: () =>
+    set({ editedTask: { id: 0, title: '', description: '' } }),
+}))
+
+export default useStore
